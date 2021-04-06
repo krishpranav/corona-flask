abcb533c2217a91e015ead6ba04b80b8ba24cbac
krishpranav <krisna.pranav@gmail.com>
2021-04-06 16:53:34 +0530

    done changes

--- city_info.py
+++ city_info.py
@@ -5,3 +5,7 @@ class city_info:
 		pass
 
 
+def get_places(city_name):
+	list_of_citites = get_list_of_citites()
+	list_of_places = get_list_of_places()
+	list_of_places_per_city = {list_of_cities[index]: [] for index in range(len(list_of_cities))}
